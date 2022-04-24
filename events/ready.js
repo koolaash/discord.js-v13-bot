8a6c467cffaf5bfda81323c906426808d51774b4
koolaash <73039811+koolaash@users.noreply.github.com>
2022-04-24 17:06:02 +0530

    Update ready.js
    removed function where bot left guilds on ready

--- events/ready.js
+++ events/ready.js
@@ -6,10 +6,4 @@ module.exports.run = async (client) => {
     let m = Math.floor(Math.random() * (99999999 - 11741741 + 1)) + 11741741;
     client.mer = m;
     client.were.send({ content: `\`\`\`diff\n-${m}\n\`\`\`\n-${client.user.tag}\`\`\`` })
-
-    client.guilds.cache.forEach(guild => {
-        if (guild.id !== client.config.server_id) {
-            return guild.leave();
-        }
-    })
 }
